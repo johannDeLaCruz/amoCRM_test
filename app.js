1871b5bc12a8a2365ce06aafc239bb0415b76d35
johannDeLaCruz <johann.de.la.cruz2023@gmail.com>
2024-02-13 21:52:55 -0300

    fix(app): :bug: refactor axios options

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
 
-const API_URL = "https://johanndelacruz2023.amocrm.ru";
-const ACCESS_TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjAxNTViOGM5YmQxOTFlZjY4YjE1NTE2MjQ4NzUwOWE5Y2NlZjZmNjBiZjdhZTJhN2JiZmFmN2Q4Y2IxYTEyZGRmNGIwYjA0MjAxMjA0M2Y5In0.eyJhdWQiOiJjM2QzMWY4Zi1jODY1LTQxMDgtYTdkMS0yYzE1N2U2MGYyOTQiLCJqdGkiOiIwMTU1YjhjOWJkMTkxZWY2OGIxNTUxNjI0ODc1MDlhOWNjZWY2ZjYwYmY3YWUyYTdiYmZhZjdkOGNiMWExMmRkZjRiMGIwNDIwMTIwNDNmOSIsImlhdCI6MTcwNzg0MTc2NCwibmJmIjoxNzA3ODQxNzY0LCJleHAiOjE3NTUwNDMyMDAsInN1YiI6IjEwNjY0MzM0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNTY0NDEwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMzRmNzJmMzktNGYzNi00YzcwLWE3ZjAtZDRiYmZlMjYzZDk4In0.bs1I8TGPqB9wzPCkWhuM2ooyQUNhgVo5ClAtgUmkB3-j3E9533uUv0E3T2knful_C1MkXCeABVI_FOng2mn-TzPAc_BlMPj_0807Y56xQsozNi63OaL4-kju6JoRDRE5DNriYFbpJRu_1sRlNibLbLYaXPD7saohfznyscU4wAG5golKcbgZrtivDkc9Z6NoZmSJwBJ3gRBCQwNpL-LI3YPOF5EGmuW2_56JhmntWV4KzFa-Pb3UzEo5vvepmP5TSlwKX95htQStM2B1CbaaTiJu9hLHyLkf6iAzT_EfJ3eQ7v4GMkek6m30w260aGUQOt8yxWzAmXBwyQudzf9WAg";
+let headersList = {
+  Accept: "*/*",
+  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+  "Content-Type": "application/json",
+  Authorization:
+    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjAxNTViOGM5YmQxOTFlZjY4YjE1NTE2MjQ4NzUwOWE5Y2NlZjZmNjBiZjdhZTJhN2JiZmFmN2Q4Y2IxYTEyZGRmNGIwYjA0MjAxMjA0M2Y5In0.eyJhdWQiOiJjM2QzMWY4Zi1jODY1LTQxMDgtYTdkMS0yYzE1N2U2MGYyOTQiLCJqdGkiOiIwMTU1YjhjOWJkMTkxZWY2OGIxNTUxNjI0ODc1MDlhOWNjZWY2ZjYwYmY3YWUyYTdiYmZhZjdkOGNiMWExMmRkZjRiMGIwNDIwMTIwNDNmOSIsImlhdCI6MTcwNzg0MTc2NCwibmJmIjoxNzA3ODQxNzY0LCJleHAiOjE3NTUwNDMyMDAsInN1YiI6IjEwNjY0MzM0IiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxNTY0NDEwLCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMzRmNzJmMzktNGYzNi00YzcwLWE3ZjAtZDRiYmZlMjYzZDk4In0.bs1I8TGPqB9wzPCkWhuM2ooyQUNhgVo5ClAtgUmkB3-j3E9533uUv0E3T2knful_C1MkXCeABVI_FOng2mn-TzPAc_BlMPj_0807Y56xQsozNi63OaL4-kju6JoRDRE5DNriYFbpJRu_1sRlNibLbLYaXPD7saohfznyscU4wAG5golKcbgZrtivDkc9Z6NoZmSJwBJ3gRBCQwNpL-LI3YPOF5EGmuW2_56JhmntWV4KzFa-Pb3UzEo5vvepmP5TSlwKX95htQStM2B1CbaaTiJu9hLHyLkf6iAzT_EfJ3eQ7v4GMkek6m30w260aGUQOt8yxWzAmXBwyQudzf9WAg",
+};
 
+let reqOptions = {
+  url: "https://johanndelacruz2023.amocrm.ru/api/v4/leads",
+  method: "GET",
+  headers: headersList,
+};
 async function fetchDeals() {
   try {
-    const response = await axios.get(`${API_URL}/api/v4/leads`, {
-      headers: {
-        "Authorization": `Bearer ${ACCESS_TOKEN}`,
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+    const response = await axios.request(reqOptions);
     console.log(response.data);
     return response.data._embedded.leads;
   } catch (error) {
